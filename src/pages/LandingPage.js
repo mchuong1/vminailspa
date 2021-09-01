2a373b3861035ae3ebd0986beb4b8bf42eff73ee
Matthew Chuong <mstchuong@yahoo.com>
2021-09-01 19:34:26 -0400

    Finish About Section

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -39,12 +39,40 @@ const useStyles = makeStyles((theme) => ({
     color: 'white',
     borderRadius: '50px',
   },
+  aboutSection: {
+    height: '93vh',
+    position: 'relative',
+    '& h1': {
+      position: 'absolute',
+      fontSize: '72px',
+    },
+    '& p': {
+      position: 'absolute',
+      top: '28vh',
+      padding: '20px',
+      width: '190px'
+    },
+    '& button': {
+      position: 'absolute',
+      marginLeft: '5px',
+      top: '46vh'
+    }
+  },
+  rectangle: {
+    backgroundColor: '#F8F6F7',
+    height: '40%',
+    width: '100%',
+    position: 'absolute',
+    top: '25vh',
+    zIndex: -1,
+  },
 }));
 
 const LandingPage = (props) => {
   const classes = useStyles();
   const { history } = props;
   const smileGirl = cld.image('VNMiNailSpa/SmileGirl');
+  const coverEyesGirl = cld.image('VNMiNailSpa/CoverEyesGirl');
 
   return (
     <div id="landingpage">
@@ -61,6 +89,18 @@ const LandingPage = (props) => {
         </div>
         <AdvancedImage cldImg={smileGirl} style={{ width: '180%', position: 'absolute', bottom: 0, left: 0 }} alt="Beautiful Girl Smile Brunette Nails"/>
         <div className={classes.circle}/>
+      </div>
+      <div className={classes.aboutSection}>
+        <h1>About Us</h1>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+        <Button
+            variant="contained"
+            classes={{ root: classes.button }}
+            >
+            Read More
+          </Button>
+        <AdvancedImage cldImg={coverEyesGirl} style={{width: '130%', right: '-20vh', bottom: '31vh', position: 'absolute'}}/>
+        <div className={classes.rectangle}/>
       </div>
     </div>
   );
