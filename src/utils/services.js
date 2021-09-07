a70fd7b69fd659122220271eb6fdf9bf9d7962ff
Matthew Chuong <mstchuong@yahoo.com>
2021-09-07 18:48:23 -0400

    Add services

---
+++ src/utils/services.js
@@ -0,0 +1,259 @@
+export const manicure = [
+  {
+    name: 'Basic',
+    description: '',
+    price: 19,
+  },
+  {
+    name: 'V and Mi - Deluxe Manicure',
+    description: '',
+    price: 37,
+  },
+  {
+    name: 'Gel Manicure',
+    description: '',
+    price: 33,
+  },
+];
+
+export const pedicure = [
+  {
+    name: 'Basic',
+    description: '',
+    price: 30,
+    time: '25 minutes',
+  },
+  {
+    name: 'V and Mi - Deluxe Pedicure',
+    description: '',
+    price: 40,
+    time: '35 minutes',
+  },
+  {
+    name: 'Organic Pedicure',
+    description: '',
+    price: 47,
+    time: '40 minutes',
+  },
+  {
+    name: 'Jelly Spa Pedicure',
+    description: '',
+    price: 60,
+    time: '50 minutes',
+  },
+];
+
+export const kidServices = [
+  {
+    name: 'Princess Manicure',
+    price: 14,
+  },
+  {
+    name: 'Princess Pedicure',
+    price: 23,
+  },
+  {
+    name: 'Polish Change - Hands',
+    price: 7,
+  },
+  {
+    name: 'Polish Change - Feet',
+    price: 10,
+  },
+];
+
+export const nailEnhancement = [
+  {
+    name: 'Solar Full Set',
+    price: '40 & Up',
+  },
+  {
+    name: 'Solor Fill Ins',
+    price: '30 & Up',
+  },
+  {
+    name: 'Solar Pink & White Full Set',
+    price: '55 & Up',
+  },
+  {
+    name: 'Solar Pink & White Fill Ins',
+    price: '45 & Up',
+  },
+  {
+    name: 'Solar Pink Fill Ins Only',
+    price: '35 & Up',
+  },
+  {
+    name: 'Liquid Gel Full Set',
+    price: '60 & Up',
+  },
+  {
+    name: 'Liquid Gel Fill Ins',
+    price: '40 & Up',
+  },
+  {
+    name: 'Liquid Gel Pink & White Full Set',
+    price: '65 & Up',
+  },
+  {
+    name: 'Liquid Gel Pink & White Fill Ins',
+    price: '55 & Up',
+  },
+  {
+    name: 'Liquid Gel Pink Fill Ins Only',
+    price: '45 & Up',
+  },
+  {
+    name: 'Cuticle Trimming',
+    price: 7,
+  },
+  {
+    name: 'Additional Gel Polish Service',
+    price: 15,
+  },
+];
+
+export const additionalServices = [
+  {
+    name: 'French Design',
+    price: '5 & Up',
+  },
+  {
+    name: 'Nail Art Design',
+    price: '5 & Up',
+  },
+  {
+    name: 'Nail Repair',
+    price: '3 & Up',
+  },
+  {
+    name: 'Remove Acrylic',
+    price: 10,
+  },
+  {
+    name: 'Remove Gel Polish - Without Service',
+    price: 10,
+  },
+  {
+    name: 'Remove Gel Polish - With Basic Manicure',
+    price: 5,
+  },
+  {
+    name: 'Polish Change - Hands',
+    price: 10,
+  },
+  {
+    name: 'Polish Change - Feet',
+    price: 14,
+  },
+  {
+    name: 'Cut Down',
+    price: 3
+  }
+];
+
+export const waxing = [
+  {
+    title: 'Eyebrows',
+    price: 12,
+  },
+
+  {
+    title: 'Upper Lip',
+    price: 8,
+  },
+  {
+    title: 'Chin',
+    price: '10 & Up',
+  },
+  {
+    title: 'Nose',
+    price: 12,
+  },
+  {
+    title: 'Ears',
+    price: 15,
+  },
+  {
+    title: 'SideBurns',
+    price: '15 & Up',
+  },
+  {
+    title: 'Full Face',
+    price: '50 & Up',
+  },
+  {
+    title: 'Half Arms',
+    price: '25 & Up',
+  },
+  {
+    title: 'Full Arms',
+    price: '40 & Up',
+  },
+  {
+    title: 'Under Arms',
+    price: '22 & Up',
+  },
+  {
+    title: 'Bikini',
+    price: '30 & Up',
+  },
+  {
+    title: 'Brazilian',
+    price: '50 & Up',
+  },
+  {
+    title: 'Half Legs',
+    price: '35 & Up',
+  },
+  {
+    title: 'Full Legs',
+    price: '55 & Up',
+  },
+  {
+    title: 'Back',
+    price: '40 & Up',
+  },
+];
+
+export const addOnServices = [
+  {
+    name: 'Paraffin Treament - Hands',
+    price: 8
+  },
+  {
+    name: 'Paraffin Treament - Feet',
+    price: 10
+  },
+  {
+    name: 'Gel Polish Change - Hands',
+    price: 25
+  },
+  {
+    name: 'Gel Polish Change - Feet',
+    price: 30
+  }
+];
+
+export const dippingPowder = [
+  {
+    name: 'Dipping Powder Full Set',
+    price: '40 & Up',
+  },
+  {
+    name: 'Dipping Powder Pink & White Full Set',
+    price: '50 & Up',
+  },
+  {
+    name: 'Ombre Full Set',
+    price: '55 & Up',
+  },
+  {
+    name: 'Add Almond/Coffin/Stiletto shape',
+    price: 3
+  },
+  {
+    name: 'Add with Tip',
+    price: 5
+  }
+]
