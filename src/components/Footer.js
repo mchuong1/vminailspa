ed81190a90f269af7fedddf9b099eed75e408ac4
Matthew Chuong <mstchuong@yahoo.com>
2021-09-11 12:16:11 -0400

    update phone and mail
    remove social media icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core';
-import FacebookIcon from '@material-ui/icons/Facebook';
-import InstagramIcon from '@material-ui/icons/Instagram';
+// import FacebookIcon from '@material-ui/icons/Facebook';
+// import InstagramIcon from '@material-ui/icons/Instagram';
 import PhoneIcon from '@material-ui/icons/Phone';
 import MailIcon from '@material-ui/icons/Mail';
 
@@ -39,10 +39,10 @@ const Footer = () => {
   return (
     <div id='footer' className={classes.footer}>
       <div className={classes.socialBar}>
-        <FacebookIcon classes={{root: classes.socialIcon}}  onClick={() => goToSocial('https://www.facebook.com/Tipsy-Nailbar-Boca-105414531836108')}/>
-        <InstagramIcon classes={{root: classes.socialIcon}} onClick={() => goToSocial('https://www.instagram.com/tipsybocaraton/?hl=en')}/>
-        <PhoneIcon classes={{root: classes.socialIcon}} onClick={() => goToSocial('tel:561.419.7911')}/>
-        <MailIcon classes={{root: classes.socialIcon}} onClick={() => goToSocial('mailto:tipsynailbarboca@gmail.com')}/>
+        {/* <FacebookIcon classes={{root: classes.socialIcon}}  onClick={() => goToSocial('https://www.facebook.com/Tipsy-Nailbar-Boca-105414531836108')}/>
+        <InstagramIcon classes={{root: classes.socialIcon}} onClick={() => goToSocial('https://www.instagram.com/tipsybocaraton/?hl=en')}/> */}
+        <PhoneIcon classes={{root: classes.socialIcon}} onClick={() => goToSocial('tel:561.367.1590')}/>
+        <MailIcon classes={{root: classes.socialIcon}} onClick={() => goToSocial('mailto:vminailspa@gmail.com')}/>
       </div>
       <span>Copyright &copy; 2021 V&#38;Mi Nail Spa | All Rights Reserved</span>
       <span>Web Development by: <span className={classes.link} aria-hidden='true' onClick={() => goToSocial('https://mattchu-portfolio.netlify.app/')}>Matthew Chuong</span></span>
