1ee00a259010ea19c06247829ae6ddb94317e3c9
Matthew Chuong <mstchuong@yahoo.com>
2021-09-11 12:14:38 -0400

    Update photos.js

--- src/utils/photos.js
+++ src/utils/photos.js
@@ -52,6 +52,56 @@ export const photos = [
     width: 2,
     height: 1,
   },
+  {
+    src: 'https://res.cloudinary.com/dgpijcm0x/image/upload/v1631376471/VNMiNailSpa/Check_In.jpg',
+    width: 1,
+    height: 1,
+  },
+  {
+    src: 'https://res.cloudinary.com/dgpijcm0x/image/upload/v1631376474/VNMiNailSpa/Front_bar.jpg',
+    width: 2,
+    height: 1,
+  },
+  {
+    src: 'https://res.cloudinary.com/dgpijcm0x/image/upload/v1631376479/VNMiNailSpa/Bar_view_2.jpg',
+    width: 2,
+    height: 1,
+  },
+  {
+    src: 'https://res.cloudinary.com/dgpijcm0x/image/upload/v1631376481/VNMiNailSpa/Bar_view_.jpg',
+    width: 2,
+    height: 1,
+  },
+  {
+    src: 'https://res.cloudinary.com/dgpijcm0x/image/upload/v1631376487/VNMiNailSpa/Nail_Tables.jpg',
+    width: 2,
+    height: 1,
+  },
+  {
+    src: 'https://res.cloudinary.com/dgpijcm0x/image/upload/v1631376497/VNMiNailSpa/Nail_Tables_2.jpg',
+    width: 2,
+    height: 1,
+  },
+  {
+    src: 'https://res.cloudinary.com/dgpijcm0x/image/upload/v1631376504/VNMiNailSpa/Nails_Table_01.jpg',
+    width: 1,
+    height: 1,
+  },
+  {
+    src: 'https://res.cloudinary.com/dgpijcm0x/image/upload/v1631376509/VNMiNailSpa/Pedicure_01.jpg',
+    width: 2,
+    height: 1,
+  },
+  {
+    src: 'https://res.cloudinary.com/dgpijcm0x/image/upload/v1631376511/VNMiNailSpa/Receiption_01.jpg',
+    width: 2,
+    height: 1,
+  },
+  {
+    src: 'https://res.cloudinary.com/dgpijcm0x/image/upload/v1631376512/VNMiNailSpa/Nails_Tables_3.jpg',
+    width: 1,
+    height: 1,
+  }
 ];
 
 export const carousel = [
