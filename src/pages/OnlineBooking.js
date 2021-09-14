5ff7022481b47d4571c318719d3d2752f35633e7
Matthew Chuong <mstchuong@yahoo.com>
2021-09-13 20:51:49 -0400

    fix booking being too tall

--- src/pages/OnlineBooking.js
+++ src/pages/OnlineBooking.js
@@ -13,7 +13,7 @@ const useStyles = makeStyles({
     // top: '15vh',
     // left: 0,
     // width: '100%',
-    height: '100%',
+    height: '90%',
     textAlign: 'center',
     // eslint-disable-next-line no-dupe-keys
     textAlign: '-webkit-center',
