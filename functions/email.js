d4934255396a1106849451ffb69f04bbbab97fda
Matthew Chuong <mstchuong@yahoo.com>
2021-09-14 22:53:27 -0400

    Use mailgun instead of sendgrid

--- functions/email.js
+++ functions/email.js
@@ -1,40 +1,24 @@
+const mailgun = require('mailgun-js');
 
-// using Twilio SendGrid's v3 Node.js Library
-// https://github.com/sendgrid/sendgrid-nodejs
-const sendgrid = require('@sendgrid/mail')
-
-module.exports.handler = async (event,) => {
-
-  if(process.env.SENDGRID_API_KEY) {
-    return {status: 500, body: 'SENDGRID_API_KEY is not set'}
-  }
-
-  sendgrid.setApiKey(process.env.REACT_APP_SENDGRID_API_KEY)
-  sendgrid.setTimeout(30000);
-  const {name, message, email} = JSON.parse(event.body)
-  const msg = {
-    to: 'vminailspa@gmail.com', // Change to your recipient
-    from: 'support@vnminailspa.com', // Change to your verified sender
-    subject: `Get in Touch: ${name}`,
-    text: `${message} - Reply back to ${email}`
-  }
-  sendgrid
-    .send(msg)
-    .then(() => {
-      console.log('Email sent')
-    })
-    .catch((error) => {
-      console.error(error)
-      return {
-        status: 500,
-        body: error
-      }
-    })
-    return {
-      statusCode: 200,
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: 'Email sent'
-    }
-}
+module.exports.handler = async (event) => {
+  const { name, message, email } = JSON.parse(event.body);
+  const DOMAIN = 'vnminailspa.com';
+  const mg = mailgun({
+    apiKey: process.env.REACT_APP_MAILGUN_API_KEY,
+    domain: DOMAIN,
+  });
+  const data = {
+    from: 'V&Mi Nail Spa <support@vnminailspa.com>',
+    to: 'vminailspa@gmail.com',
+    subject: 'Hello',
+    text: `${name} Testing some Mailgun awesomness! ${message} ${email}`,
+  };
+  mg.messages().send(data, (error, body) => {
+    if (error) console.error(error);
+    else console.log(body);
+  });
+  return {
+    statusCode: 200,
+    body: 'Email Sent Successfully',
+  };
+};
